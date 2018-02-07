fd4c97e83eba82c39ca2b151482de25e0564ae5a
ZJXin <ls_zjxin0708@126.com>
2018-02-07 16:08:51 +0800

    完善显示天气信息功能。包括添加一张背景图以及将背景图和系统状态栏进行融合。

--- app/src/main/java/com/coolweather/ui/WeatherActivity.java
+++ app/src/main/java/com/coolweather/ui/WeatherActivity.java
@@ -1,18 +1,22 @@
 package com.coolweather.ui;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Scroller;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.coolweather.R;
 import com.coolweather.gson.Forecast;
 import com.coolweather.gson.Weather;
@@ -40,10 +44,20 @@ public class WeatherActivity extends BaseActivity {
     private TextView comfortTV;
     private TextView carWashTV;
     private TextView sportTV;
+    private ImageView bingPicImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //将背景图和系统状态栏融合一起
+        if (Build.VERSION.SDK_INT >= 21){
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_weather);
         //获取控件实例
         initView();
@@ -58,6 +72,12 @@ public class WeatherActivity extends BaseActivity {
             String weatherId = getIntent().getStringExtra("weather_id");
             weatherLayout.setVisibility(View.INVISIBLE);
             requestWeather(weatherId);
+        }
+        String bingPic = prefs.getString("bing_pic",null);
+        if (bingPic != null){
+            Glide.with(this).load(bingPic).into(bingPicImg);
+        }else {
+            loadBingPic();
         }
     }
 
@@ -76,6 +96,7 @@ public class WeatherActivity extends BaseActivity {
         comfortTV = (TextView)findViewById(R.id.comfort_text);
         carWashTV = (TextView)findViewById(R.id.car_wash_text);
         sportTV = (TextView)findViewById(R.id.sport_text);
+        bingPicImg = (ImageView)findViewById(R.id.bing_pic_img);
     }
 
     /**
@@ -117,6 +138,31 @@ public class WeatherActivity extends BaseActivity {
                             Toast.makeText(WeatherActivity.this,
                                     "获取天气信息失败",Toast.LENGTH_SHORT).show();
                         }
+                    }
+                });
+            }
+        });
+    }
+
+    public void loadBingPic(){
+        final String requestBingPic = "http://guolin.tech/api/bing_pic";
+        HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String bingPic = response.body().string();
+                SharedPreferences.Editor editor = PreferenceManager.
+                        getDefaultSharedPreferences(WeatherActivity.this).edit();
+                editor.putString("bing_pic",bingPic);
+                editor.apply();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.with(WeatherActivity.this).load(bingPic).into(bingPicImg);
                     }
                 });
             }
